6741545b723664c5d88047d3649b2654934edf88
Alumno <Alumno@ws206-09>
2019-02-08 09:53:26 -0800

    Creación del proyecto.

---
+++ Paquete/src/test/Pruebas.java
@@ -0,0 +1,16 @@
+package test;
+
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		paquete1.Persona p=new paquete1.Persona(24,"Andro");
+		paquete2.Persona m=new paquete2.Persona(21, "Juan", "Masculino");
+		
+		System.out.println(p.toString());
+		System.out.println(m.toString());
+		
+	}
+
+}
